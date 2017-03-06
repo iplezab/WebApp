f6ca99f94b16a3ff2d360399023f03ed63521097
iplezab <plely26@gmail.com>
2017-03-06 15:42:25 +0700

    https://github.com/iplezab/WebApp.git

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+include ("Header.php");
+echo "<div align='center'>";
+echo "<h1 style='color: salmon'>Playground Shop</h1>";
+echo "<img src='animal.png' width='50%' height='50%'>";
+echo "<br/>";
+echo "</div>";
+include ("Footer.php");
+show_source("Header.php");
+show_source("home.php");
+show_source("Footer.php");
+?>
